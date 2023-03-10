82d926ec332833811a836f117daa70b7061c98e2
auhyh <heidi.au@hertfordshire.gov.uk>
2023-03-10 11:04:17 +0000

    remove prof tiles

--- directory/time.R
+++
@@ -1,8 +0,0 @@
-library(profvis)
-profvis({ runApp('/cloud/project') }  
-        , prof_output = '/cloud/project/directory')
-
-profvis(prof_input = '/cloud/project/directory/filee6357d6794.Rprof') 
-
-p <- profvis(prof_input = '/cloud/project/directory/filee6357d6794.Rprof')
-htmlwidgets::saveWidget(p, "/cloud/project/directory/diagnosis.html")
